a3d3030157a13ff72956c3f77166d517e0b3a6a6
ThiagoSiSa <santos.auto@hotmail.com>
2021-11-21 14:32:41 -0300

    Adicionada funçao para receber e guardar categorias e titulos

--- register.rb
+++ register.rb
@@ -1,8 +1,19 @@
 class Register
     attr_accessor :title, :category
+    require 'json'
 
     def initialize(title='Geral',category='Geral')
         @title=title
         @category=category
+
+        file = File.read('/home/thiago/challenge/data.json')
+        reg = JSON.parse(file)
+        
+        reg["data"].push({"category" => category, "title" => title})
+
+        File.open("/home/thiago/challenge/data.json","w") do |f|
+            f.write(reg.to_json)
+          end
+        InitialPage.new
     end
 end
